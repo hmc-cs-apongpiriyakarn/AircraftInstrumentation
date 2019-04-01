27c5425ed1353b885d338b16fda24479a908e27c
hmc-cs-apongpiriyakarn <35592070+hmc-cs-apongpiriyakarn@users.noreply.github.com>
2019-03-31 17:48:16 -0700

    Update adxl345spi.c

--- adxl345spi.c
+++ adxl345spi.c
@@ -126,7 +126,7 @@ int main() {
             testz = (data[6]<<8)|data[5];
             
             if((abs(testx-lx)>30) || (abs(testy-ly)>30) || (abs(testz-lz)>30)) {
-                printf("x=%d y=%d z=%d\n", testx, testy, testz);
+                printf("new\nx=%d y=%d z=%d\n", testx, testy, testz);
                 lx = testx;
                 ly = testy;
                 lz = testz;
@@ -147,7 +147,7 @@ int main() {
  data[0], data[1], data[2], data[3], data[4], data[5], data[6]);
         
         
-        printf("test\ntestx = %.3f, testy = %.3f, testz = %.3f\n",
+        printf("testx = %.3f, testy = %.3f, testz = %.3f\n",
  x * accConversion, y * accConversion, z * accConversion);
         rawx = ((data[2] & 0x0F)<<8)|data[1];
         rawy = ((data[4] & 0x0F)<<8)|data[3];
@@ -160,8 +160,8 @@ int main() {
         accely = (rawy/256.0) - 16*signy;
         accelz = (rawz/256.0) - 16*signz;
         
-        printf("raw\nrawx = %x rawy = %x rawz = %x\n", rawx, rawy, rawz);
-        printf("time: %.3f, x = %.9f, y = %.9f, z = %.9f\n", t, accelx, accely, accelz);
+        printf("rawx = %x rawy = %x rawz = %x\n", rawx, rawy, rawz);
+        printf("time: %.3f, x = %.9f, y = %.9f, z = %.9f\n\n", t, accelx, accely, accelz);
         delayMillis(200);
     }
     
