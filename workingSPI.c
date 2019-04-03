cb107316dd4a365525cef682eb95cae74ce71baa
hmc-cs-apongpiriyakarn <35592070+hmc-cs-apongpiriyakarn@users.noreply.github.com>
2019-04-03 18:43:10 +0000

    Update workingSPI.c

--- workingSPI.c
+++ workingSPI.c
@@ -51,15 +51,15 @@ int main() {
         //bytes = readBytes(h, data, 7);
         bytes = spiSendReceiveBytes(data, 7);
         printf("data[0]: %x \ndata[1]: %x \ndata[2]: %x \ndata[3]: %x \ndata[4]: %x \ndata[5]: %x \ndata[6]: %x \n",
-//  data[0], data[1], data[2], data[3], data[4], data[5], data[6]);
-        //if (bytes == 7) {
+            data[0], data[1], data[2], data[3], data[4], data[5], data[6]);
+        if (bytes == 7) {
             x = (data[2]<<8)|data[1];
             y = (data[4]<<8)|data[3];
             z = (data[6]<<8)|data[5];
             t = time_time() - tStart;
             printf("time = %.3f, x = %.3f, y = %.3f, z = %.3f\n",
                    t, x*2*16.0/8192.0, y*2*16.0/8192.0, z*2*16.0/8192.0);
-        //}
+        }
         delayMillis(200);
     }
     tDuration = time_time() - tStart; 
