8c7905328de9dd04ac21d539012609293b9ffe0a
hmc-cs-apongpiriyakarn <35592070+hmc-cs-apongpiriyakarn@users.noreply.github.com>
2019-04-03 18:53:37 +0000

    Update workingSPI.c

--- workingSPI.c
+++ workingSPI.c
@@ -50,7 +50,7 @@ int main() {
         data[0] = DATAX0;
         bytes = readBytes(h, data, 7);
         //bytes = spiSendReceiveBytes(data, 7);
-        printf("data[0]: %x \nx0: %x x1: %x \ny0: %x y1: %x \nz0: %x z1: %x \n",
+        printf("data[0]: %x \nx0: %x \tx1: %x \ny0: %x \ty1: %x \nz0: %x \tz1: %x \n",
             data[0], data[1], data[2], data[3], data[4], data[5], data[6]);
         if (bytes == 7) {
             x = (data[2]<<8)|data[1];
