c2eaabae6f54ac23e6dafb5145fb282c578f8c9d
hmc-cs-apongpiriyakarn <35592070+hmc-cs-apongpiriyakarn@users.noreply.github.com>
2019-04-16 10:14:37 -0700

    Update workingSPI.c

--- workingSPI.c
+++ workingSPI.c
@@ -126,7 +126,7 @@ void logData(void) {
 // 		samples[sample][2] = low | (high<<8);
 
 		readADXL345(sample);
-		printf("sample num: %d, x = %.3f, y = %.3f, z = %.3f, micros: %d\n\n",
+		printf("sample num: %d, x = %.3f, y = %.3f, z = %.3f, micros: %f\n\n",
 			   sample, 
 			   samples[sample][0]*2*16.0/8192.0, 
 			   samples[sample][1]*2*16.0/8192.0, 
@@ -143,12 +143,13 @@ void logData(void) {
 			// time to write to file
 			fprintf(fptr, "%s\n", tbuf);
 			for(int i=0; i<SAMPLESPERINTERVAL; i++) {
-				fprintf(fptr, "x = %.3f, y = %.3f, z = %.3f, t: %d\n", 
+				fprintf(fptr, "x = %.3f, y = %.3f, z = %.3f",// t: %f\n", 
 					samples[i][0]*2*16.0/8192.0, 
 					samples[i][1]*2*16.0/8192.0, 
-					samples[i][2]*2*16.0/8192.0,
-					samples[i][3]);
+					samples[i][2]*2*16.0/8192.0;
+					//samples[i][3]);
 					count++;
+				fprintf(fptr, " t: %f\n", samples[i][3]);
 			}
 			fflush(fptr); // make sure write completes
 			getDateTime(); // update time for next interval
